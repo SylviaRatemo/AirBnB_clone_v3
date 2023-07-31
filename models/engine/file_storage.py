5f54ffa2c0babd3fb6e307e4f4d63c2b5c07b015
SylviaRatemo <ratemosylvia@gmail.com>
2023-07-31 06:10:00 -0700

    states views

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -71,7 +71,7 @@ class FileStorage:
 
     def get(self, cls, id):
         """method to retrieve a single object"""
-        key = cls.__name__ + "." + str(id)
+        key = cls + "." + str(id)
         return self.__objects.get(key)
 
     def count(self, cls=None):
