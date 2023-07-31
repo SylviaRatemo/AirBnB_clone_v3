7ae14582e61aaf104851da31cc1b1b5e8602bba7
SylviaRatemo <ratemosylvia@gmail.com>
2023-07-31 06:16:56 -0700

    states

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """Module for Blueprint app_views"""
 from flask import Blueprint, abort, make_response
-from api.v1.views.index import *
-from api.v1.views.states import *
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+
+from api.v1.views.index import *
+from api.v1.views.states import *
 
 
 def get_models(parent_model, parent_model_id, parent_getter):
