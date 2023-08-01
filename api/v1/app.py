f4ed2c2202647dad072fad0d6d92b5b24d6abbd1
SylviaRatemo <ratemosylvia@gmail.com>
2023-08-01 02:31:14 -0700

    body wrong

--- api/v1/app.py
+++ api/v1/app.py
@@ -19,7 +19,7 @@ def closedb(foo):
 
 @app.errorhandler(404)
 def not_found(error):
-    return make_response(jsonify({'error': 'Not Found'}), 404)
+    return make_response(jsonify({'error': 'Not Found' }), 404)
 
 if __name__ == "__main__":
     host = getenv("HBNB_API_HOST", "0.0.0.0")
